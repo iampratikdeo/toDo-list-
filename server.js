4b5739bb96695db02000d2b040b797992ff0120b
pratik Deo <pdeo1@stevens.edu>
2020-07-27 17:50:21 -0400

    update date string added ul

--- server.js
+++ server.js
@@ -2,32 +2,31 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", function(req, res){
   var today = new Date();
   var currentDay = today.getDay();
   var day = "";
   var days = ["Sunday","Monday", "tuesday", "Wednesday", "Thursday", "Friday",
               "Saturday"];
-  // if(today.getDay() === 6 || today.getDate() === 0){
-  //   day = "weekend";
-  //   res.render("list", {KindOfDay: day});
-  //
-  // }else {
-  //   day = "weekday";
-  //   res.render("list", {KindOfDay: day});
-  // }
 
-  for(var i = 0; i < 7; i++){
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
 
-    if(i == currentDay){
-      day = days[i];
-      break;
-    }
-  }
-  console.log(today);
-  res.render("list", {KindOfDay:day});
+  var day_new = today.toLocaleDateString("en-US", options);
 
+  res.render("list", {KindOfDay:day_new});
+
+});
+
+app.post("/", function(req,res){
+  var item = req.body.newItem;
+  console.log(item);
 });
 
 app.listen("8080", function(){
