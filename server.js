fa7738acef06ddff7a73f7cf58d43dd63d1c2174
pratik Deo <pdeo1@stevens.edu>
2020-07-27 17:14:10 -0400

    support for days

--- server.js
+++ server.js
@@ -2,15 +2,31 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-
+app.set('view engine', 'ejs');
 app.get("/", function(req, res){
   var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
+  var days = ["Sunday","Monday", "tuesday", "Wednesday", "Thursday", "Friday",
+              "Saturday"];
+  // if(today.getDay() === 6 || today.getDate() === 0){
+  //   day = "weekend";
+  //   res.render("list", {KindOfDay: day});
+  //
+  // }else {
+  //   day = "weekday";
+  //   res.render("list", {KindOfDay: day});
+  // }
 
-  if(today.getDay() === 6 || today.getDate() === 0){
-    res.send("<h1>Yay it's the weekend!</h1>")
-  }else {
-    res.send("<h1>Boo! It's a weekday</h1>");
+  for(var i = 0; i < 7; i++){
+
+    if(i == currentDay){
+      day = days[i];
+      break;
+    }
   }
+  console.log(today);
+  res.render("list", {KindOfDay:day});
 
 });
 
